2809a5d89fe2ad388093f53aeead78a910c354e9
Sanjeev Kumar <106345083+Sanjeevkumar98@users.noreply.github.com>
2025-05-14 23:52:09 +0530

    Delete test/unit directory

--- test/unit/test_handler.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from src.handler import lambda_handler
-
-class TestLambdaHandler(unittest.TestCase):
-    def test_lambda_handler(self):
-        result = lambda_handler({}, None)
-        self.assertEqual(result['statusCode'], 200)
